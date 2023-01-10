ae20e1acf98666a89caf4538c23eb62ffc10a29d
wigeo <wigeo.developer@gmail.com>
2023-01-10 13:10:12 -0500

    Solved new leetcode problem "Same Tree"

--- src/leetcode/SameTree.java
+++ src/leetcode/SameTree.java
@@ -3,7 +3,7 @@ package leetcode;
 import leetcode.util.TreeNode;
 
 /*
-    Link: https://leetcode.com/problems/same-tree
+    Link: https://leetcode.com/problems/same-tree/
 
     Given the roots of two binary trees p and q, write a function to check if they are the same or not.
 
@@ -17,29 +17,33 @@ import leetcode.util.TreeNode;
         Input: p = [1,2], q = [1,null,2]
         Output: false
 
-    Example 3:
+    Example 3
         Input: p = [1,2,1], q = [1,1,2]
         Output: false
-
  */
 
 public class SameTree {
+    private boolean result = true;
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null || q == null){
-            return (p == null && q == null) ? true : false;
+        checkSameTreeByNode(p, q);
+
+        return result;
+    }
+
+    private void checkSameTreeByNode(TreeNode p, TreeNode q) {
+        if ((p == null && q != null) || (p != null && q == null)) {
+            result = false;
+            return;
         }
-        if(p.val != q.val){
-            return false;
+        if(p == null && q == null){
+            return;
         }
-        boolean leftCheck = isSameTree(p.left, q.left);
-        if(!leftCheck){
-            return false;
+        if (p.val == q.val){
+            checkSameTreeByNode(p.left , q.left);
+            checkSameTreeByNode(p.right, q.right);
+        }else{
+            result = false;
         }
-        boolean rightCheck = isSameTree(p.right, q.right);
-        if(!rightCheck){
-            return false;
-        }
-        return true;
     }
 }
